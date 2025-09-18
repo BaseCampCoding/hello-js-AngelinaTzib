05b528250041e358dd87d27cc6227a5381a7e201
AngelinaTzib <366789@northwestms.edu.>
2025-09-18 15:21:21 -0500

    went over the assignment and added notes

--- 7-Loops/jasmine/src/main.js
+++ 7-Loops/jasmine/src/main.js
@@ -1,9 +1,10 @@
 // Task 1
-let birdsPerDay = [1, 3, 5];
+let birdsPerDay = [5, 2, 6, 7, 9, 10, 20];
 let total = 0;
 for (let i = 0; i < birdsPerDay.length; i++){
     total += birdsPerDay[i];
 }
+// i++ means it will add 1 to i each time it loops
 console.log(total);
 
 
@@ -14,12 +15,13 @@ let weekStart = (week - 1) * daysPerWeek;
 let weekEnd = week * daysPerWeek;
 let weekTotal = 0;
 for (let i = weekStart; i < weekEnd && i < birdsPerDay.length; i++){
-    weekTotal + birdsPerDay[i];
+    weekTotal += birdsPerDay[i];
 }
-console.log()
+console.log(weekTotal);
 
 
 // Task 3
 for (let i = 0; i < birdsPerDay.length; i += 2){
     birdsPerDay[i] += 1
 }
+console.log(birdsPerDay);
