d4d27210ecc14343ff2f11fcf5f857b2806f4a11
AngelinaTzib <366789@northwestms.edu.>
2025-09-18 09:48:52 -0500

    added console.log and notes

--- 4-Boolean/jasmine/src/main.js
+++ 4-Boolean/jasmine/src/main.js
@@ -5,18 +5,26 @@ let prisonerIsAwake = true;
 let petDogIsPresent = true;
 
 
-
 // task 1
 let canExecuteFastAttack = !knightIsAwake;
+console.log('Fast Attack: ' + canExecuteFastAttack);
+// false
 
 
 // task 2
 let canSpy = knightIsAwake || archerIsAwake || prisonerIsAwake;
+console.log('Spy: ' + canSpy)
+// true
 
 
 // task 3
-let canSignalPrisoner = prisonerIsAwake && archerIsAwake;
+let canSignalPrisoner = prisonerIsAwake && !archerIsAwake;
+console.log('Signal Prisoner: ' + canSignalPrisoner)
+// true
 
 
 // task 4
-let canFreePrisoner = petDogIsPresent && !archerIsAwake || !petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake;
+let canFreePrisoner = (petDogIsPresent && !archerIsAwake) || 
+(!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
+console.log('Free Prisoner: ' + canFreePrisoner);
+// true
