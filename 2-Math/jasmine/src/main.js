b14fb2a06aef8358838de8a9e0e3156e30e59a93
AngelinaTzib <366789@northwestms.edu.>
2025-09-19 10:39:17 -0500

    saving my progress on this assignment

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -4,14 +4,18 @@ let dayRate = ratePerHour * 8;
 
 let budget = 1000;
 let workdays = Math.floor(budget / dayRate);
+// Math.floor is to round down //
 
 
 let billable = 22;
-let monthlyDiscount = 0.2;
+// let monthlyDiscount = 0.2;
+ let monthlyDiscount = (billable * dayRate) * .2
 
 let fullMonths = Math.floor(35 / billable);
 let remainingDays = 35 - billable;
+// let remainingDays = 35 - billable; //
 
-let discountedMonthlyRate = dayRate * billable * (1 - monthlyDiscount);
+// let discountedMonthlyRate = dayRate * billable * (1 - monthlyDiscount);
+let discountedMonthlyRate = (billable * dayRate) - monthlyDiscount
 let remainingCost = remainingDays * dayRate;
-let totalProjectCost = Math.ceil (discountedMonthlyRate + remainingCost);
+let totalProjectCost = Math.ceil(discountedMonthlyRate + remainingCost);
---
+++ 9-Objects/jasmine/src/main.js
@@ -0,0 +1,32 @@
+// task 1 //
+const scoreBoard = {
+    "The Best Ever" : 1000000
+};
+
+
+// task 2 //
+let newPlayerName = 'me';
+let newPlayerScore = 100;
+ scoreBoard[newPlayerName] = newPlayerScore;
+console.log(scoreBoard);
+
+
+// task 3 //
+let playerToRemove = 'me';
+delete scoreBoard[playerToRemove];
+console.log(scoreBoard);
+
+
+// task 4 //
+scoreboard[newPlayerName] = newPlayerScore;
+let playerNameToUpdate = 'me';
+let scoreToAdd = 50;
+scoreBoard[playerNameToUpdate] = newPlayerScore + scoreToAdd;
+console.log(scoreBoard);
+
+
+// task 5 //
+let mondayBonus = 100;
+for (let i = 0;){
+    scoreBoard;
+}
