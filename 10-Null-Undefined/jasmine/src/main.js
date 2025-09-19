e49de5ca9e0df0d6cc841dcc94de2e5f7a7ecb52
AngelinaTzib <366789@northwestms.edu.>
2025-09-19 12:45:16 -0500

    saving changes

---
+++ 10-Null-Undefined/jasmine/src/main.js
@@ -0,0 +1,21 @@
+// task 1 //
+let visitorName;
+let visitorAge;
+let visitorTicketId;
+const visitor = {
+    name, age, ticketId
+}
+
+
+// task 2 //
+ticketId [visitor = null]
+
+
+// task 3 //
+const tickets = {
+    ticketId
+}
+let ticketStatusResult
+
+
+
