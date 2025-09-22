90517321e1ad0331249d91c9264293b261b34081
AngelinaTzib <366789@northwestms.edu.>
2025-09-22 10:58:52 -0500

    went over assignment

--- 10-Null-Undefined/jasmine/src/main.js
+++ 10-Null-Undefined/jasmine/src/main.js
@@ -1,21 +1,56 @@
 // task 1 //
-let visitorName;
-let visitorAge;
-let visitorTicketId;
-const visitor = {
-    name, age, ticketId
+let visitorName = "Kaden";
+let visitorAge = 45;
+let visitorTicketId = "Tic48";
+let visitor = {
+    name: visitorName, 
+    age: visitorAge,
+    ticketId: visitorTicketId
 }
+console.log(visitor);
 
 
 // task 2 //
-ticketId [visitor = null]
-
+visitor.ticketId = null;
+console.log(visitor);
 
 // task 3 //
-const tickets = {
-    ticketId
+let tickets = {
+    "Ticket48": null,
+    "Ticket49": "Kaden"
 }
-let ticketStatusResult
+let ticketIdToCheck = "Ticket49";
+let ticketStatusResult;
+if(!(ticketIdToCheck in tickets)){
+    ticketStatusResult = "unknown ticket id"
+}
+else if(tickets[ticketIdToCheck] == null){
+    ticketStatusResult = "not sold"
+}
+else{
+    ticketStatusResult = "sold to " + tickets[ticketIdToCheck]
+}
+console.log(ticketStatusResult);
 
 
+// task 4 //
+let simpleTicketStatusResult
+if(ticketIdToCheck in tickets && tickets[ticketIdToCheck]){
+    simpleTicketStatusResult = tickets[ticketIdToCheck]
+}
+else{
+    simpleTicketStatusResult = "invalid ticket"
+}
+console.log(simpleTicketStatusResult);
 
+
+// task 5 //
+let visitorWithGtc = {
+    name: "Kaden",
+    gtc: {version: "v2.0"}
+}
+let gtcVersion;
+if(visitorWithGtc.gtc && visitorWithGtc.gtc.version){
+    gtcVersion = visitorWithGtc.gtc.version;
+}
+console.log(gtcVersion);
