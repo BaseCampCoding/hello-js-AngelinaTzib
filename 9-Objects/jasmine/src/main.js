a8f9825b663f188504782f870c626f1b58cf31eb
AngelinaTzib <366789@northwestms.edu.>
2025-09-22 10:07:33 -0500

    went over assignment

--- 9-Objects/jasmine/src/main.js
+++ 9-Objects/jasmine/src/main.js
@@ -1,32 +1,30 @@
 // task 1 //
-const scoreBoard = {
-    "The Best Ever" : 1000000
-};
+const scoreBoard = {"The Best Ever" : 1000000, "Kobe": 7000};
 
 
 // task 2 //
-let newPlayerName = 'me';
-let newPlayerScore = 100;
- scoreBoard[newPlayerName] = newPlayerScore;
+let newPlayerName = 'Lebron';
+let newPlayerScore = 5000;
+scoreBoard[newPlayerName] = newPlayerScore;
 console.log(scoreBoard);
 
 
 // task 3 //
-let playerToRemove = 'me';
+let playerToRemove = 'The Best Ever';
 delete scoreBoard[playerToRemove];
 console.log(scoreBoard);
 
 
 // task 4 //
-scoreboard[newPlayerName] = newPlayerScore;
-let playerNameToUpdate = 'me';
-let scoreToAdd = 50;
-scoreBoard[playerNameToUpdate] = newPlayerScore + scoreToAdd;
+let playerNameToUpdate = "Lebron";
+let scoreToAdd = 1000;
+scoreBoard[playerNameToUpdate] += scoreToAdd;
 console.log(scoreBoard);
 
 
-// task 5 //
+// // task 5 //
 let mondayBonus = 100;
-for (let i = 0;){
-    scoreBoard;
+for (let player in scoreBoard){
+    scoreBoard[player] += mondayBonus;
 }
+console.log(scoreBoard);
